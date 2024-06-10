9957fecfffae192d6259be2b4c868f59b0e85d78
rafaeldtr41 <rafaeldtr41@gmail.com>
2024-06-10 19:56:41 +0200

    actualizado el admin

--- virtual_store/Book/admin.py
+++ virtual_store/Book/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from Book.models import Book_File, Book, Author
+from Book.models import *
 
 admin.site.register(Book_File)
 admin.site.register(Book)
 admin.site.register(Author)
+admin.site.register(Image_Book)
+admin.site.register(Pre_saved_PDF)
+admin.site.register(Preview_Book_File)
